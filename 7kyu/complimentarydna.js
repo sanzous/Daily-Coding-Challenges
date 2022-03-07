6c20434d087298fb39af9af0aeceb202a1723c20
sanzous <canastraleonard123@gmail.com>
2022-03-07 08:05:15 -0600

    8kyu cockroachspeed

---
+++ 7kyu/complimentarydna.js
@@ -0,0 +1,14 @@
+function DNAStrand(dna) {
+    dna = Array.from(dna)
+    for (let i = 0; i < dna.length; i++) {
+        if (dna[i] == 'A') {
+            dna[i] = 'T'
+        } else if (dna[i] == 'T') {
+            dna[i] = 'A'
+        } if (dna[i] == 'C') {
+            dna[i] = 'G'
+        } else if (dna[i] == 'G') {
+            dna[i] = 'C'
+        }
+    } return dna.join('')
+}  
--- 8kyu/cockroachspeed.js
+++ 8kyu/cockroachspeed.js
@@ -1,3 +1,15 @@
+//PREP
+
+//Parameters is the speed of the cockroach as a number
+
+
+//example 
 function cockroachSpeed(s) {
     return Math.floor(s * 100000 / 3600)
+
+
+    //psuedocode
+    //times the km by 100000 for cm then divide by how many seconds in an hour
 }
+
+//Return is the speed needed in the form of a number
