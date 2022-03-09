45bd241a91355bfb317b926c5f92781d0ad292c4
sanzous <canastraleonard123@gmail.com>
2022-03-09 07:56:38 -0600

    8kyu repeatstring

---
+++ 8kyu/repeatstringntimes.js
@@ -0,0 +1,11 @@
+//PREP
+//Parameters will be a number to repeat the string that many times
+
+function repeatStr(n, s) {
+    //Example 6, 'i' will return iiiiii
+    //Psuedo Code
+    //use the repeat method on s, n times
+    return s.repeat(n);
+
+    //Return the return will be the string repeated n times
+}
