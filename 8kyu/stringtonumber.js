377bfff21f26880ecde80f83113e3f30ecaafb7a
sanzous <canastraleonard123@gmail.com>
2022-03-10 12:10:16 -0600

    Added 6 Kyu

---
+++ 8kyu/stringtonumber.js
@@ -0,0 +1,10 @@
+// PREP
+// Parameters. I will be passed a number in the form of a string. 
+const stringToNumber = function (str) {
+    // Example. If given a string '1' what will be returned is 1 the number
+    // PsuedoCode 
+    //   Take string and use either Number or parseInt and then return
+    return parseInt(str);
+
+    //Return. What will be returned is the string turned into a number
+}
