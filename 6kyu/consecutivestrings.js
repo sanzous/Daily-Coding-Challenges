663383a34da7cd2630faa9a87875252ee5d1d094
[Leonard Canastra] <[canastraleonard123@gmail.com]>
2022-05-07 11:59:41 -0500

    new project

---
+++ 6kyu/consecutivestrings.js
@@ -0,0 +1,17 @@
+function longestConsec(strarr, k) {
+
+    if (strarr.length == 0 || k > strarr.length || k <= 0) {
+        return "";
+    }
+    let newstrarr = [];
+    for (let i = 0; i < strarr.length - k + 1; i++) {
+        let newword = [];
+        for (let j = 0; j < k; j++) {
+            newword += strarr[i + j]
+
+
+        } newstrarr.push(newword)
+
+    }
+    return newstrarr.reduce((r, e) => r.length < e.length ? e : r, "");
+}
