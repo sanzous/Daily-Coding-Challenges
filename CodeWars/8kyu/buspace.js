5e478b0b3484b84d70923068e3f123f9bf80c838
sanzous <canastraleonard123@gmail.com>
2022-07-29 16:20:09 -0500

    8kyu

---
+++ CodeWars/8kyu/buspace.js
@@ -0,0 +1,10 @@
+// You have to write a function that accepts three parameters:
+
+// cap is the amount of people the bus can hold excluding the driver.
+// on is the number of people on the bus excluding the driver.
+// wait is the number of people waiting to get on to the bus excluding the driver.
+// If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.
+
+function enough(cap, on, wait) {
+    return (cap - on) >= wait ? 0 : (cap - on - wait) * -1;
+}
