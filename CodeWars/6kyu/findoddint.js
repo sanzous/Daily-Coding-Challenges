bc7245fc598b90353ff7fc22156447533e2e9dde
sanzous <canastraleonard123@gmail.com>
2022-08-17 09:25:08 -0500

    6kyu

---
+++ CodeWars/6kyu/findoddint.js
@@ -0,0 +1,11 @@
+// Given an array of integers, find the one that appears an odd number of times.
+
+// There will always be only one integer that appears an odd number of times.
+
+function findOdd(A) {
+    let count = {};
+    A.forEach(v => {
+        count[v] = count[v] ? count[v] + 1 : 1;
+    });
+    return +Object.keys(count).find(key => count[key] % 2 === 1);
+}
