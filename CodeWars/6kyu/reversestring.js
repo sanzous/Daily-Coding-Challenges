e49ed5de9b8840023fa362f453e611deb3607434
sanzous <canastraleonard123@gmail.com>
2022-08-29 13:31:05 -0500

    6kyu

---
+++ CodeWars/6kyu/reversestring.js
@@ -0,0 +1,8 @@
+// Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+function spinWords(string) {
+    let splitstring = string.split(' ')
+    let splittedwords = splitstring.map(e => e.split(''))
+    splittedwords.map(e => e.length >= 5 ? e.reverse() : 0)
+    return splittedwords.map(e => e.join('')).join(' ')
+}
