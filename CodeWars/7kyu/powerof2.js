7502ba125e1e26e3b93b7d55d91d770922c754fb
sanzous <canastraleonard123@gmail.com>
2022-09-09 09:15:36 -0500

    7kyu

---
+++ CodeWars/7kyu/powerof2.js
@@ -0,0 +1,16 @@
+// Complete the function power_of_two/powerOfTwo (or equivalent, depending on your language) that determines if a given non-negative integer is a power of two. From the corresponding Wikipedia entry:
+
+// a power of two is a number of the form 2n where n is an integer, i.e. the result of exponentiation with number two as the base and integer n as the exponent.
+
+// You may assume the input is always valid.
+
+function isPowerOfTwo(n) {
+    if (n == 0)
+        return false;
+    while (n != 1) {
+        if (n % 2 != 0)
+            return false;
+        n = n / 2;
+    }
+    return true;
+}
