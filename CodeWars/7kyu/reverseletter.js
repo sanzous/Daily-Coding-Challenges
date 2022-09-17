7c70c952cb9af60b8eb8df389fdb34d10d1acd3b
sanzous <canastraleonard123@gmail.com>
2022-09-17 13:04:55 -0500

    7kyu

---
+++ CodeWars/7kyu/reverseletter.js
@@ -0,0 +1,17 @@
+// Given a string str, reverse it and omit all non-alphabetic characters.
+
+// Example
+// For str = "krishan", the output should be "nahsirk".
+
+// For str = "ultr53o?n", the output should be "nortlu".
+
+// Input/Output
+// [input] string str
+// A string consists of lowercase latin letters, digits and symbols.
+
+// [output] a string
+
+function reverseLetter(str) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    return str.toLowerCase().split('').filter(e => alphabet.includes(e)).reverse().join('')
+}
