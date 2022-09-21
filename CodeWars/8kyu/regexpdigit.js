1e5185733e0d4ccfe82af23aaf671900e2de656e
sanzous <canastraleonard123@gmail.com>
2022-09-21 12:30:58 -0500

    8kyu

---
+++ CodeWars/8kyu/regexpdigit.js
@@ -0,0 +1,5 @@
+// Implement String#digit? (in Java StringUtils.isDigit(String)), which should return true if given object is a digit (0-9), false otherwise.
+
+String.prototype.digit = function () {
+    return /^\d$/.test(this)
+};
