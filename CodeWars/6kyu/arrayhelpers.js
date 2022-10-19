cc0f8a20f9f3ea0329e8726864c395ee507a14c9
sanzous <canastraleonard123@gmail.com>
2022-10-19 11:10:28 -0500

    6kyu

---
+++ CodeWars/6kyu/arrayhelpers.js
@@ -0,0 +1,55 @@
+
+// This kata is designed to test your ability to extend the functionality of built-in classes. In this case, we want you to extend the built-in Array class with the following methods: square(), cube(), average(), sum(), even() and odd().
+
+// Explanation:
+
+// square() must return a copy of the array, containing all values squared
+// cube() must return a copy of the array, containing all values cubed
+// average() must return the average of all array values; on an empty array must return NaN (note: the empty array is not tested in Ruby!)
+// sum() must return the sum of all array values
+// even() must return an array of all even numbers
+// odd() must return an array of all odd numbers
+// Note: the original array must not be changed in any case!
+
+// Example
+// var numbers = [1, 2, 3, 4, 5];
+
+// numbers.square();  // must return [1, 4, 9, 16, 25]
+// numbers.cube();    // must return [1, 8, 27, 64, 125]
+// numbers.average(); // must return 3
+// numbers.sum();     // must return 15
+// numbers.even();    // must return [2, 4]
+// numbers.odd();     // must return [1, 3, 5]
+
+
+//P -- an array of numbers
+//R -- The return of all new prototyped methods for array
+//E -- [1, 2, 3, 4, 5] should return 
+// [1, 4, 9, 16, 25]);
+// [1, 8, 27, 64, 125]);
+// 15
+// 3
+// [2, 4]
+// [1, 3, 5]
+//P -- First we create an Array.prototype.{name} for each new methoed
+// Then its easy as using already previous built in javascript methods to provide the wanted return
+// or using loops to provide the wanted asnwer
+// We then return on each this object of each new prototype
+Array.prototype.square = function () {
+    return this.map(v => v * v)
+}
+Array.prototype.cube = function () {
+    return this.map(v => v * v * v)
+}
+Array.prototype.sum = function () {
+    return this.reduce((a, b) => a + b, 0)
+}
+Array.prototype.average = function () {
+    return this.reduce((a, b) => a + b, 0) / this.length
+}
+Array.prototype.even = function () {
+    return this.filter(v => v % 2 == 0)
+}
+Array.prototype.odd = function () {
+    return this.filter(v => v % 2 !== 0)
+}
--- CodeWars/7kyu/mostdigits.js
+++ CodeWars/7kyu/mostdigits.js
@@ -2,3 +2,13 @@
 
 // If two numbers in the argument array have the same number of digits, return the first one in the array.
 
+//P -- An array of numbers.
+//R -- The element in the array that contaisn the most amount of digits. If there are multiple elements return the first one in the array by index
+//E -- [1, 10, 100], 100
+//P -- First we create a new variable holding the array parameter that has been mapped where each element will be modified with the toString method
+// We then sort the new array by length and return the first element of that new array
+
+function findLongest(array) {
+    let newarr = array.map(e => e.toString())
+    return +newarr.sort((a, b) => b.length - a.length)[0]
+}
