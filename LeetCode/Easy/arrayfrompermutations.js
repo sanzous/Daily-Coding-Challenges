c210db1ecd781256cc7537306df66588d02fd947
sanzous <canastraleonard123@gmail.com>
2022-11-11 10:04:02 -0600

    leetcode easy

--- LeetCode/Easy/arrayfrompermutations.js
+++ LeetCode/Easy/arrayfrompermutations.js
@@ -6,6 +6,10 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+//P - An array of distinct integers
+//R - An array of the same length where the newarr[i] = paramater[paramater[i]]
+//E - [0,2,1,5,3,4], [0,1,2,4,5,3]
+//P - we can use a map method on the given array paramater, inside the map method we give the parameters 
 var buildArray = function (nums) {
 
     return nums.map((e, i, a) => a[a[i]])
