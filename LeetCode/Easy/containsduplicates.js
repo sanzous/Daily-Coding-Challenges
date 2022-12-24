1a390542e9e35836f3650b399c5c79d7be79f88f
sanzous <canastraleonard123@gmail.com>
2022-12-23 23:49:11 -0600

    LeetCode Easy  challenge completed

---
+++ LeetCode/Easy/containsduplicates.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+    let hash = {}
+    for (let i = 0; i < nums.length; i++) {
+        if (!hash[nums[i]]) {
+            hash[nums[i]] = true
+        } else if (hash[nums[i]]) {
+            return true
+        }
+    } return false
+};
