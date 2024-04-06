f6672c98e19a5f351ba35cfd5687bddc66cd440f
Leonard Canastra <canastraleonard123@gmail.com>
2024-04-06 16:24:45 -0500

    LeetCode Medium completed

---
+++ LeetCode/Medium/maximumavgsubarray.js
@@ -0,0 +1,31 @@
+// You are given an integer array nums consisting of n elements, and an integer k.
+
+// Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,12,-5,-6,50,3], k = 4
+// Output: 12.75000
+// Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+// Example 2:
+
+// Input: nums = [5], k = 1
+// Output: 5.00000
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function(nums, k) {
+    var sum = 0;
+   for (let i=0; i<k; i++) sum += nums[i];
+   var res = sum;
+   for (let i=k; i<nums.length; i++) {
+       sum += nums[i] - nums[i-k];
+       res = Math.max(res, sum);
+   }
+   return res/k;
+};
